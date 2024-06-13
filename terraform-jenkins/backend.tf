7a8a202701cc3aadd63ec6314561ca75c8d267a4
Aravind Jayaprakash <58030148+arvindsetty@users.noreply.github.com>
2024-06-13 12:36:07 +0530

    Update backend.tf

--- terraform-jenkins/backend.tf
+++ terraform-jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terra.tfstate" # Replace with your actual S3 bucket name
+    bucket = "eksterraform1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terra.tfstate"
     region = "ap-south-1"
   }
